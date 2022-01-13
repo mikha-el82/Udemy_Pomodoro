8951f46a58aec19a68b76e0d26408e14758bacd3
mikha-el <mikhael@centrum.cz>
2022-01-13 19:06:26 +0100

    Finished

--- main.py
+++ main.py
@@ -6,14 +6,38 @@ RED = "#e7305b"
 GREEN = "#519259"
 YELLOW = "#fffda2"
 FONT_NAME = "Courier"
-WORK_MIN = 1 # 25
-SHORT_BREAK_MIN = 2 # 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 30
 reps = 0
+timer = ""
+timer_running = False
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+
+def reset_timer():
+    global reps
+    reps = 0
+    global timer_running
+    timer_running = False
+    window.after_cancel(timer)
+    timer_txt.config(text="Timer", fg=GREEN)
+    checkmarks_txt.config(text="")
+    canvas.itemconfig(timer_text, text="00:00")
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+def timer_check():
+    global timer_running
+    if not timer_running:
+        timer_running = True
+        start_timer()
+
+
 def start_timer():
     global reps
     reps += 1
@@ -30,20 +54,29 @@ def start_timer():
         countdown(work_sec)
         timer_txt.config(text="Work", fg=GREEN)
 
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
 def countdown(count):
-    min = count // 60
-    sec = count % 60
-    if sec < 10:
-        sec = f"0{sec}"
-    time_left = f"{min}:{sec}"
+    mins = count // 60
+    secs = count % 60
+    if secs < 10:
+        secs = f"0{secs}"
+    time_left = f"{mins}:{secs}"
     canvas.itemconfig(timer_text, text=time_left)
     if count > 0:
-        window.after(1000, countdown, count-1)
+        global timer
+        timer = window.after(1000, countdown, count - 1)
     else:
         start_timer()
+        work_cycles = reps // 2
+        ticks = work_cycles * "✔"
+        checkmarks_txt.config(text=ticks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Pomodoro Counter")
 window.config(padx=100, pady=50, bg=YELLOW)
@@ -56,13 +89,13 @@ canvas.grid(column=1, row=1)
 timer_txt = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
 timer_txt.grid(column=1, row=0)
 
-start_btn = Button(text="Start", font=(FONT_NAME, 20, "bold"), highlightthickness=0, command=start_timer)
+start_btn = Button(text="Start", font=(FONT_NAME, 20, "bold"), highlightthickness=0, command=timer_check)
 start_btn.grid(column=0, row=2)
 
-reset_btn = Button(text="Reset", font=(FONT_NAME, 20, "bold"), highlightthickness=0)
+reset_btn = Button(text="Reset", font=(FONT_NAME, 20, "bold"), highlightthickness=0, command=reset_timer)
 reset_btn.grid(column=2, row=2)
 
-checkmarks_txt = Label(text="✔", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 20, "bold"))
+checkmarks_txt = Label(bg=YELLOW, fg=GREEN, font=(FONT_NAME, 20, "bold"))
 checkmarks_txt.grid(column=1, row=3)
 
 window.mainloop()
